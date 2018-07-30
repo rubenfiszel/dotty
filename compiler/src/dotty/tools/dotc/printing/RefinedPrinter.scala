3817733430e99acbe09204a9f45eeb3381aead12
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-07-30 21:10:09 +0200

    Fix #4068 properly
    `argText` will never add parentheses around its output, which makes sense for
    arguments of type constructors: if we're printing `Foo[T1, T2]` we never
    need parens around `T1` or `T2`, whatever its root operator.
    
    But that's inappropriate for a function type `T1 => T2`; the code in
    `toTextFunction` attempted to use `atPrec(InfixPrec)` to correct for that, but
    that didn't work, because `argText` called `atPrec(GlobalPrec)` right away,
    hence we ended up running
    `atPrec(InfixPrec) { atPrec(GlobalPrec) { ... }}`, which means just
    `atPrec(GlobalPrec) { ... }`.

--- compiler/src/dotty/tools/dotc/printing/RefinedPrinter.scala
+++ compiler/src/dotty/tools/dotc/printing/RefinedPrinter.scala
@@ -135,10 +135,8 @@ class RefinedPrinter(_ctx: Context) extends PlainPrinter(_ctx) {
     def toTextFunction(args: List[Type], isImplicit: Boolean, isErased: Boolean): Text =
       changePrec(GlobalPrec) {
         val argStr: Text =
-          if (args.length == 2 && defn.isFunctionType(args.head)) {
-            "(" ~ globalPrecArgText(args.head) ~ ")"
-          } else if (args.length == 2 && !defn.isTupleType(args.head))
-            atPrec(InfixPrec) { globalPrecArgText(args.head) }
+          if (args.length == 2 && !defn.isTupleType(args.head))
+            atPrec(InfixPrec) { argText(args.head) }
           else
             toTextTuple(args.init)
         (keywordText("erased ") provided isErased) ~ (keywordText("implicit ") provided isImplicit) ~ argStr ~ " => " ~ globalPrecArgText(args.last)
